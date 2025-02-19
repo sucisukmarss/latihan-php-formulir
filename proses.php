0774eba26bf2ebb9cf5c6c288b49c9f624b4eecc
sucisukmarss <ratnasuci.work@gmail.com>
2025-02-19 18:36:51 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,14 +1,15 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    $nama = $_POST["nama"];
-    $alamat = $_POST["alamat"];
-    $email = $_POST["email"];
-    $telepon = $_POST["telepon"];
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+  $nama = $_POST["nama"];
+  $alamat = $_POST["alamat"];
+  $email = $_POST["email"];
+  $telepon = $_POST["telepon"];
     
-    echo "<h2>Data yang Diterima:</h2>";
-    echo "Nama Lengkap:" .$nama. "<br>";
-    echo "Alamat:" .$alamat. "<br>";
-    echo "Email:" .$email. "<br>";
-    echo "Nomor Telepon:" .$telepon. "<br>";
+  echo "<h2>Data yang Diterima:</h2>";
+  echo "Nama Lengkap:" .$nama. "<br>";
+  echo "Alamat:" .$alamat. "<br>";
+  echo "Email:" .$email. "<br>";
+  echo "Nomor Telepon:" .$telepon. "<br>";
 }
 ?>
